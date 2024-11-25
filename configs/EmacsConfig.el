d64271e1d9302e918de2f9dbc9f0567d3acd49a0
Mario Kostadinov <mkostadinov@callflowlab.com>
2024-11-25 13:01:51 +0200

    asd

--- configs/EmacsConfig.el
+++ configs/EmacsConfig.el
@@ -43,6 +43,9 @@
 		eshell-mode-hook))
   (add-hook mode (lambda() (display-line-numbers-mode 0))))
 
+(use-package all-the-icons)
+;; After this we need to the M-x all-the-icons-install-fonts
+
 (use-package doom-themes
   :ensure t
   :config
@@ -79,35 +82,35 @@
       ("~/mos/notes/tasks/Inbox.org" :maxlevel . 1)))
 
 (setq org-agenda-start-with-log-mode t)
-          (setq org-log-done 'time)
-          (setq org-log-into-drawer t)
-          (setq org-agenda-span 14)
-          (setq org-todo-keywords
-              '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELLED")))
-        (setq org-agenda-custom-commands
-              '(("p" "Projects" tags-todo "+project")
-                ("mt" "This month view"
-            ((agenda "" 
-                      ((org-agenda-span 'month)  ;; Set the agenda span to one month
-                     (org-agenda-start-day (format-time-string "%Y-%m-01")
-                       (org-agenda-start-on-weekday nil)  ;; Don't force the start day to be Monday
-                       ))))
-                )
-("mp" "Previous Month"
-           ((agenda ""
-                    ((org-agenda-span 'month)
-                     (org-agenda-start-day (format-time-string "%Y-%m-01" (time-subtract (current-time) (days-to-time 30))))
-                     (org-agenda-show-all-dates t)
-                     (org-agenda-start-on-weekday nil)))))
+(setq org-log-done 'time)
+(setq org-log-into-drawer t)
+(setq org-agenda-span 14)
+(setq org-todo-keywords
+      '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELLED")))
+(setq org-agenda-custom-commands
+      '(("p" "Projects" tags-todo "+project")
+        ("mt" "This month view"
+         ((agenda "" 
+                  ((org-agenda-span 'month)  ;; Set the agenda span to one month
+                   (org-agenda-start-day (format-time-string "%Y-%m-01")
+                                         (org-agenda-start-on-weekday nil)  ;; Don't force the start day to be Monday
+                                         ))))
+         )
+        ("mp" "Previous Month"
+         ((agenda ""
+                  ((org-agenda-span 'month)
+                   (org-agenda-start-day (format-time-string "%Y-%m-01" (time-subtract (current-time) (days-to-time 30))))
+                   (org-agenda-show-all-dates t)
+                   (org-agenda-start-on-weekday nil)))))
 
- ("mn" "Next Month"
+        ("mn" "Next Month"
          ((agenda ""
                   ((org-agenda-span 'month)
                    (org-agenda-start-day (format-time-string "%Y-%m-01"
                                                              (time-add (current-time) (days-to-time 30))))
                    (org-agenda-show-all-dates t)
                    (org-agenda-start-on-weekday nil)))))
-                ))
+        ))
 
 (use-package org
   :hook (org-mode . efs/org-mode-setup))
@@ -123,33 +126,33 @@
 ;; Make sure org-indent face is available
 (require 'org-indent)
 
-  ;; Increase the size of various headings
-  (set-face-attribute 'org-document-title nil :font "Cantarell" :weight 'bold :height 1.3)
+;; Increase the size of various headings
+(set-face-attribute 'org-document-title nil :font "Cantarell" :weight 'bold :height 1.3)
 
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Cantarell" :weight 'medium :height (cdr face)))
+(dolist (face '((org-level-1 . 1.2)
+                (org-level-2 . 1.1)
+                (org-level-3 . 1.05)
+                (org-level-4 . 1.0)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font "Cantarell" :weight 'medium :height (cdr face)))
 
-  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
-  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+;; Ensure that anything that should be fixed-pitch in Org files appears that way
+(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
+(set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+(set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
 
-  ;; Get rid of the background on column views
-  (set-face-attribute 'org-column nil :background nil)
-  (set-face-attribute 'org-column-title nil :background nil)
+;; Get rid of the background on column views
+(set-face-attribute 'org-column nil :background nil)
+(set-face-attribute 'org-column-title nil :background nil)
 
 (use-package org-bullets
 :after org
@@ -163,8 +166,8 @@
 
 (use-package visual-fill-column
     :config
-      (setq-default visual-fill-column-width 100)
-      (setq-default visual-fill-column-center-text t)
+      (setq-default visual-fill-column-width 150)
+      (setq-default visual-fill-column-center-text t  )
     )
 
 
@@ -203,30 +206,21 @@
          :target (file+head "~/mos/notes/OrgRoam/projects/${slug}.org"
                             "#+title: ${title}\n#+category: ${title}\n#+filetags: project ${slug}\n* Overview\n- Start date: %U\n- Current status: \n\n* Tasks\n\n* Milestones\n")
          :unnarrowed t)
-("r" "Recipes" plain "%?"
+        ("r" "Recipes" plain "%?"
          :if-new (file+head "~/mos/notes/recipes/${slug}.org"
                             "#+title: ${title}\n#+filetags: recipes\n#+date: %U\n\n* Ingredients\n\n* Instructions")
          :unnarrowed t)
         ))
 
 (setq org-roam-dailies-capture-templates
-          '(("d" "default" entry "* %?"
-             :target (file+head+olp "%<%Y-%m-%d>.org"
+      '(("d" "default" entry "* %?"
+         :target (file+head+olp "%<%Y-%m-%d>.org"
                                 "#+title: %<%Y-%m-%d>" ("Inbox"))
-             :unnarrowed t)
+         :unnarrowed t)
 
-            ("t" "Todo")
+        ("t" "Todo")
 
-            ("tt" "Todo" entry "* TODO %^{What do you want to do?}"
-             :target (file+head+olp "%<%Y-%m-%d>.org"
-                                    "#+title: %<%Y-%m-%d>"
-                                    ("Tasks"))
-             :unnarrowed t
-             :empty-lines-before 1
-             :prepend t
-             :immediate-finish t)
-
-            ("ts" "Scheduled TODO" entry "* TODO %^{What do you want to do?}\nSCHEDULED: %^t"
+        ("tt" "Todo" entry "* TODO %^{What do you want to do?}"
          :target (file+head+olp "%<%Y-%m-%d>.org"
                                 "#+title: %<%Y-%m-%d>"
                                 ("Tasks"))
@@ -234,41 +228,50 @@
          :empty-lines-before 1
          :prepend t
          :immediate-finish t)
- ("l" "Website" entry "* [[%^{URL}][%^{Description}]]\n:PROPERTIES:\n:WEBSITE: yes\n:WEBSITE_TYPE: %^{Type of website|blog|documentation|tutorial|article|reference|ecommerce}\n:END:\nCaptured on %U"
-       :target (file+head+olp "%<%Y-%m-%d>.org"
-                              "#+title: %<%Y-%m-%d>\n"
-                              ("Websites"))
-       :unnarrowed t
-       :empty-lines-before 1
-       :prepend t
-       :immediate-finish t)
+
+        ("ts" "Scheduled TODO" entry "* TODO %^{What do you want to do?}\nSCHEDULED: %^t"
+         :target (file+head+olp "%<%Y-%m-%d>.org"
+                                "#+title: %<%Y-%m-%d>"
+                                ("Tasks"))
+         :unnarrowed t
+         :empty-lines-before 1
+         :prepend t
+         :immediate-finish t)
+        ("l" "Website" entry "* [[%^{URL}][%^{Description}]]\n:PROPERTIES:\n:WEBSITE: yes\n:WEBSITE_TYPE: %^{Type of website|blog|documentation|tutorial|article|reference|ecommerce}\n:END:\nCaptured on %U"
+         :target (file+head+olp "%<%Y-%m-%d>.org"
+                                "#+title: %<%Y-%m-%d>\n"
+                                ("Websites"))
+         :unnarrowed t
+         :empty-lines-before 1
+         :prepend t
+         :immediate-finish t)
 
 
-            ("r" "Reminder" entry "* %^{Reminder} %^T"
-             :target (file+head+olp "%<%Y-%m-%d>.org"
-                                    "#+title: %<%Y-%m-%d>\n"
-                                    ("Reminders"))
-             :unnarrowed t
-             :empty-lines-before 1
-             :prepend t
-             :immediate-finish t)
-("j" "Journal" entry "* %^{Journal: }\n:LOGBOOK:\nCLOCK: %U\n:END:"
-          :target (file+head+olp "%<%Y-%m-%d>.org"
-                                 "#+title: %<%Y-%m-%d>\n"
-                                 ("Journal"))
-          :unnarrowed t
-          :empty-lines-before 1
-          :prepend t
-          :immediate-finish t)
+        ("r" "Reminder" entry "* %^{Reminder} %^T"
+         :target (file+head+olp "%<%Y-%m-%d>.org"
+                                "#+title: %<%Y-%m-%d>\n"
+                                ("Reminders"))
+         :unnarrowed t
+         :empty-lines-before 1
+         :prepend t
+         :immediate-finish t)
+        ("j" "Journal" entry "* %^{Journal: }\n:LOGBOOK:\nCLOCK: %U\n:END:"
+         :target (file+head+olp "%<%Y-%m-%d>.org"
+                                "#+title: %<%Y-%m-%d>\n"
+                                ("Journal"))
+         :unnarrowed t
+         :empty-lines-before 1
+         :prepend t
+         :immediate-finish t)
 
-            ("w" "Work Entry" entry "* %^{What did you do?}\n:LOGBOOK:\nCLOCK: %U\n:END:"
-             :target (file+head+olp "%<%Y-%m-%d>.org"
-                                    "#+title: %<%Y-%m-%d>\n"
-                                    ("Work"))
-             :unnarrowed t
-             :empty-lines-before 1
-             :prepend t
-             :immediate-finish t)))
+        ("w" "Work Entry" entry "* %^{What did you do?}\n:LOGBOOK:\nCLOCK: %U\n:END:"
+         :target (file+head+olp "%<%Y-%m-%d>.org"
+                                "#+title: %<%Y-%m-%d>\n"
+                                ("Work"))
+         :unnarrowed t
+         :empty-lines-before 1
+         :prepend t
+         :immediate-finish t)))
 
 (use-package org-ql
           :quelpa (org-ql :fetcher github :repo "alphapapa/org-ql"
@@ -347,6 +350,15 @@
             (setq org-agenda-files (directory-files-recursively "~/mos/notes""\\.org$")))))
 
 ;;(setq org-agenda-files '("~/mos/notes"))
+
+(use-package evil
+  :init
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+)
 
 (use-package ivy
   :bind(
