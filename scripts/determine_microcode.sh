91a9b3474f13371cc69885977d20adffa4b63408
Mario Kostadinov <mkostadinov@callflowlab.com>
2024-12-05 23:05:32 +0200

    asd

--- scripts/determine_microcode.sh
+++ scripts/determine_microcode.sh
@@ -3,10 +3,10 @@ determine_microcode() {
     local cpu_type=$1
     case "$cpu_type" in
         AMD|amd)
-            CPU_MICROCODE="amd_ucode"
+            CPU_MICROCODE="amd-ucode"
             ;;
         INTEL|intel)
-            CPU_MICROCODE="intel_ucode"
+            CPU_MICROCODE="intel-ucode"
             ;;
         *)
             echo "Error: Unsupported CPU type. Use 'AMD' or 'INTEL'."
